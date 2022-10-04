b6bac8098b153c868c7bb26f45d0b63b059d257e
kyleb48 <106144512+kyleb48@users.noreply.github.com>
2022-10-04 18:06:03 +0200

    Update build_and_publish_imageV2.sh

--- build_and_publish_imageV2.sh
+++ build_and_publish_imageV2.sh
@@ -2,6 +2,6 @@
 
 echo ${REPO}/${REPO_IMAGE}:${APP_VERSION}${GO_PIPELINE_COUNTER}
 
-docker build . -f app/Dockerfile -t ${REPO}/${REPO_IMAGE}:${APP_VERSION}${GO_PIPELINE_COUNTER}
+docker build app/ -f app/Dockerfile -t ${REPO}/${REPO_IMAGE}:${APP_VERSION}${GO_PIPELINE_COUNTER}
 
 docker push ${REPO}/${REPO_IMAGE}:${APP_VERSION}${GO_PIPELINE_COUNTER}
